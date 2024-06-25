f7597d19513d243afd7860173c31721451a5f30e
Alon Ewenson <alonewenson@gmail.com>
2024-06-25 12:19:00 +0300

    fixed build error

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -1,7 +1,17 @@
-import Widgets from "@/app/widgets/page";
+import { fetchFromCommonNinja } from "../../services/api";
+import { Widget } from "@/types";
+import List from "../../components/itemList/list";
+import Layout from "../../components/layout/layout";
 
-const ProjectComponents = ({ params }: { params: { id: string } }) => {
-  return <Widgets projetId={params.id} />;
+const ProjectComponents = async ({ params }: { params: { id: string } }) => {
+  const widgets: Widget[] = await fetchFromCommonNinja(`widgets?projectId=${params.id}`);
+
+  return (
+    <Layout>
+      <List items={widgets} linkRoute="widgets" />
+    </Layout>
+  );
 };
 
 export default ProjectComponents;
+
--- app/widgets/page.tsx
+++ app/widgets/page.tsx
@@ -3,9 +3,8 @@ import { Widget } from "@/types";
 import List from "../components/itemList/list";
 import Layout from "../components/layout/layout";
 
-const Widgets = async ({ projetId }: { projetId?: string }) => {
-  const url = projetId ? `widgets?projectId=${projetId}` : "widgets";
-  const widgets: Widget[] = await fetchFromCommonNinja(url);
+const Widgets = async () => {
+  const widgets: Widget[] = await fetchFromCommonNinja('widgets');
 
   return (
     <Layout>
